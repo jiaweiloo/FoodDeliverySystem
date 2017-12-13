c5f1f4a00c5a6403fd7f369521fe900667c97944
jiaweiloo <jiawe@DESKTOP-AF7GDFA>
2017-12-14 01:49:31 +0800

    Update 14/12/2017 programming change

--- src/fooddeliverysystem/MainForm.java
+++ src/fooddeliverysystem/MainForm.java
@@ -5,9 +5,13 @@ import PART_C.SelectRestaurant;
 import PART_D.*;
 import adt.*;
 import entity.*;
+import controller.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 /**
  * @author jiaweiloo
@@ -17,11 +21,15 @@ public class MainForm extends javax.swing.JFrame {
     ListInterface<employee> empList = new LList<employee>();
     ListInterface<Attendance> attdList = new LList<Attendance>();
     LinkedQueue<Order> orderList = new LinkedQueue<Order>();
+    public LinkedQueue<Integer> employeeID = new LinkedQueue<Integer>();
+    ListInterface<emp_handled_list> ehlList = new LList<emp_handled_list>();
     employee emp, emp1, emp2, emp3, emp4, emp5;
     Attendance att, att1, att2, att3, att4, att5;
     Order ord, ord1, ord2, ord3, ord4, ord5;
+    emp_handled_list ehl;
     SimpleDateFormat timeOnly = new SimpleDateFormat("hh:mm:ss");
     SimpleDateFormat dateOnly = new SimpleDateFormat("dd/MM/yyyy");
+    deliveryManInterface DMI;
 
     /**
      * Creates new form MainForm
@@ -41,9 +49,6 @@ public class MainForm extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton6 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
         btnRegister = new javax.swing.JButton();
         btnExit = new javax.swing.JButton();
         jtfTime = new javax.swing.JTextField();
@@ -60,23 +65,6 @@ public class MainForm extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         setSize(new java.awt.Dimension(600, 300));
-
-        jButton2.setText("PART A");
-        jButton2.setActionCommand("");
-
-        jButton3.setText("PART B");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-
-        jButton4.setText("PART C");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
 
         btnRegister.setLabel("Register");
 
@@ -131,14 +119,7 @@ public class MainForm extends javax.swing.JFrame {
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addComponent(jtfEmail)
-                                        .addComponent(jpfPassword)))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGap(24, 24, 24)
-                                    .addComponent(jButton2)
-                                    .addGap(27, 27, 27)
-                                    .addComponent(jButton3)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                    .addComponent(jButton4)))
+                                        .addComponent(jpfPassword))))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(120, 120, 120)
                                 .addComponent(btnLogin)
@@ -158,12 +139,7 @@ public class MainForm extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton3)
-                    .addComponent(jButton2)
-                    .addComponent(jButton4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addContainerGap(84, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblTime)
                     .addComponent(jtfTime, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -187,22 +163,6 @@ public class MainForm extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
-        SelectRestaurant restaurant = new SelectRestaurant();
-        restaurant.setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-        // New_Delivery_Man newD;
-        //newD = new New_Delivery_Man();
-        // newD.setVisible(true);
-        // this.dispose();
-        Update_Delivery_Men.main(null);
-    }//GEN-LAST:event_jButton3ActionPerformed
 
     private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
         System.exit(0);        // TODO add your handling code here:
@@ -234,8 +194,7 @@ public class MainForm extends javax.swing.JFrame {
             {
                 startEmployeeInterface();
                 this.setVisible(false);
-            }
-            else{
+            } else {
                 JOptionPane.showMessageDialog(null, "Login success, application under construction! ");
             }
 
@@ -285,25 +244,55 @@ public class MainForm extends javax.swing.JFrame {
             }
         });
 
+        Timer timer = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //jtfTime.setText(new Date().toString());
+                mainform.runThis();
+            }
+        });
+        timer.start();
         //new MainForm().setVisible(true);
     }
 
+    private void runThis() {
+        jtfTime.setText(new Date().toString());
+        //loadAvailableEmployee();
+        int handle_id = 000001;
+
+        if (!employeeID.isEmpty() && !orderList.isEmpty()) {
+            if (!ehlList.isEmpty()) {
+                handle_id = ehlList.getEntry(ehlList.getNumberOfEntries()).getHandle_id() + 1;
+            }
+            ehl = new emp_handled_list(handle_id, employeeID.getFront(), orderList.getFront().getOrder_id(), dateOnly.format(new Date()), timeOnly.format(new Date()), "HANDLED", "NONE");
+            ehlList.add(ehl);
+            if (emp.getEmp_id() == employeeID.getFront()) {
+                DMI.nextOrder(orderList.getFront());
+            }
+            orderList.dequeue();
+            employeeID.dequeue();
+        }
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+        }
+    }
+
     private void updateList() {
-        emp1 = new employee(100001, "jason@mail.com", "abcd1234", "available", "890831-05-4492", "A-4-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4441221", "DM");
-        emp2 = new employee(100002, "jack@mail.com", "abcd1234", "offline", "890731-05-4492", "A-7-4 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4661321", "EXEC");
-        emp3 = new employee(100003, "annabelle@mail.com", "abcd1234", "available", "800831-05-4592", "A-3-6 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-8535221", "DM");
-        emp4 = new employee(100004, "marie@mail.com", "abcd1234", "delivering", "990731-08-4492", "A-2-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-1231221", "DM");
+        emp1 = new employee(100001, "jason@mail.com", "abcd1234", "offline", "890831-05-4492", "A-4-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4441221", "DM");
+        emp2 = new employee(100002, "jack@mail.com", "abcd1234", "other", "890731-05-4492", "A-7-4 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4661321", "EXEC");
+        emp3 = new employee(100003, "annabelle@mail.com", "abcd1234", "offline", "800831-05-4592", "A-3-6 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-8535221", "DM");
+        emp4 = new employee(100004, "marie@mail.com", "abcd1234", "offline", "990731-08-4492", "A-2-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-1231221", "DM");
         emp5 = new employee(100005, "lucas@mail.com", "abcd1234", "other", "790821-05-4492", "A-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4990621", "AFFT");
         att1 = new Attendance(600001, 100001, "21/07/2017", "08:00:21", "17:03:21", "13:10:52", "13:55:13");
         att2 = new Attendance(600002, 100002, "21/07/2017", "08:01:11", "17:13:31", "13:05:51", "14:02:11");
         att3 = new Attendance(600003, 100001, "22/07/2017", "08:11:31", "17:23:41", "13:12:25", "14:01:12");
         att4 = new Attendance(600004, 100002, "22/07/2017", "08:05:41", "17:02:51", "13:13:15", "14:11:33");
         att5 = new Attendance(600005, 100001, "23/07/2017", "07:59:51", "17:01:01", "13:11:25", "14:02:23");
-        ord1 = new Order(200001,  "LOI KAH HOU", "014-2233445" , "loikh-wa15@student.tarc.edu.my" ,  "A-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL",  300001, 25.50 , 2 , "PENDING" );
-        ord2 = new Order(200002,  "LIM JUN KIT ", "012-3311221" , "limjk-wa15@student.tarc.edu.my" ,  "A-9-5 Teratai Residency, Jln Genting Klang, 53300 KL",  300002, 35.50 , 4 , "PENDING" );
-        ord3 = new Order(200003,  "MAH HONG WAI", "014-3311311" , "mahhw-wa15@student.tarc.edu.my" ,  "A-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL",  300001, 30.50 , 2 , "PENDING" );
-        ord4 = new Order(200004,  " LIM NAN FUNG", "014-1235437" , "limnf-wa15@student.tarc.edu.my" ,  "B-6-5 Teratai Residency, Jln Genting Klang, 53300 KL",  300002, 17.50 , 1 , "PENDING" );
-        ord5 = new Order(200005,  "LIM PENG LEN", "013-22211122" , "limpl-wa15@student.tarc.edu.my" ,  "C-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL",  300002, 22.50 , 3 , "PENDING" );
+        ord1 = new Order(200001, "LOI KAH HOU", "014-2233445", "loikh-wa15@student.tarc.edu.my", "A-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL", 300001, 25.50, 2, "PENDING");
+        ord2 = new Order(200002, "LIM JUN KIT ", "012-3311221", "limjk-wa15@student.tarc.edu.my", "A-9-5 Teratai Residency, Jln Genting Klang, 53300 KL", 300002, 35.50, 4, "PENDING");
+        ord3 = new Order(200003, "MAH HONG WAI", "014-3311311", "mahhw-wa15@student.tarc.edu.my", "A-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL", 300001, 30.50, 2, "PENDING");
+        ord4 = new Order(200004, " LIM NAN FUNG", "014-1235437", "limnf-wa15@student.tarc.edu.my", "B-6-5 Teratai Residency, Jln Genting Klang, 53300 KL", 300002, 17.50, 1, "PENDING");
+        ord5 = new Order(200005, "LIM PENG LEN", "013-22211122", "limpl-wa15@student.tarc.edu.my", "C-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL", 300002, 22.50, 3, "PENDING");
         empList.add(emp1);
         empList.add(emp2);
         empList.add(emp3);
@@ -322,7 +311,7 @@ public class MainForm extends javax.swing.JFrame {
     }
 
     public void startEmployeeInterface() {
-        deliveryManInterface DMI = new deliveryManInterface(empList, attdList, emp, orderList);
+        DMI = new deliveryManInterface(empList, attdList, emp, orderList);
         DMI.updateAttendance(att);
         //DMI.updateEmp(emp);
         DMI.setVisible(true);
@@ -340,9 +329,6 @@ public class MainForm extends javax.swing.JFrame {
     private javax.swing.JButton btnFgtPw;
     private javax.swing.JButton btnLogin;
     private javax.swing.JButton btnRegister;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPasswordField jpfPassword;
