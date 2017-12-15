d50df7f94c49ef63c1d0a60f7065f9cbe54b3a30
jiaweiloo <jiawe@DESKTOP-AF7GDFA>
2017-12-15 14:25:51 +0800

    Update 15/12/2017 Updated

--- src/fooddeliverysystem/MainForm.java
+++ src/fooddeliverysystem/MainForm.java
@@ -17,10 +17,10 @@ import javax.swing.Timer;
  */
 public class MainForm extends javax.swing.JFrame {
 
-    ListInterface<employee> empList = new LList<employee>();
+    EmployeeInterface<employee> empList = new EmployeeADT<employee>();
     ListInterface<Attendance> attdList = new LList<Attendance>();
     public LinkedQueue<Order> orderList = new LinkedQueue<Order>();
-    public LinkedQueue<Integer> employeeID = new LinkedQueue<Integer>();
+    public LinkedQueue<Integer> empWaitingList = new LinkedQueue<Integer>();
     ListInterface<emp_handled_list> ehlList = new LList<emp_handled_list>();
     employee emp, emp1, emp2, emp3, emp4, emp5;
     Attendance att, att1, att2, att3, att4, att5;
@@ -214,23 +214,25 @@ public class MainForm extends javax.swing.JFrame {
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
         // TODO add your handling code here:
-        //Loop and get everything in mylist and compare the object in the list with the textbox field
         boolean success = false;
-        for (int a = 1; a < empList.getNumberOfEntries(); a++) {
-            emp = empList.getEntry(a);
-            //verified email and password
-            if (emp.getEmail().equals(jtfEmail.getText()) && emp.getPassword().equals(jpfPassword.getText())) {
-                att = new Attendance(attdList.getEntry(attdList.getNumberOfEntries()).getAttendance_id() + 1,
-                        emp.getEmp_id(), dateOnly.format(new Date()), timeOnly.format(new Date()), "00:00:00", "00:00:00", "00:00:00");
+        if(empList.searchString(jtfEmail.getText())!= null){
+            emp = empList.searchString(jtfEmail.getText());
+            if (emp.getPassword().equals(jpfPassword.getText())) {
+                att = new Attendance(
+                        attdList.getEntry(attdList.getNumberOfEntries()).getAttendance_id() + 1,
+                        emp.getEmp_id(), 
+                        dateOnly.format(new Date()), 
+                        timeOnly.format(new Date()), 
+                        "00:00:00", 
+                        "00:00:00",
+                        "00:00:00");
                 attdList.add(att);
-
                 success = true;
-                break;
             } else {
                 success = false;
             }
         }
-
+        
         if (!success) {
             JOptionPane.showMessageDialog(null, "Verified fail! Please try again");
         } else {
@@ -320,17 +322,17 @@ public class MainForm extends javax.swing.JFrame {
         //loadAvailableEmployee();
         int handle_id = 000001;
 
-        if (!employeeID.isEmpty() && !orderList.isEmpty()) {
+        if (!empWaitingList.isEmpty() && !orderList.isEmpty()) {
             if (!ehlList.isEmpty()) {
                 handle_id = ehlList.getEntry(ehlList.getNumberOfEntries()).getHandle_id() + 1;
             }
-            ehl = new emp_handled_list(handle_id, employeeID.getFront(), orderList.getFront().getOrder_id(), dateOnly.format(new Date()), timeOnly.format(new Date()), "HANDLED", "NONE");
+            ehl = new emp_handled_list(handle_id, empWaitingList.getFront(), orderList.getFront().getOrder_id(), dateOnly.format(new Date()), timeOnly.format(new Date()), "HANDLED", "NONE");
             ehlList.add(ehl);
-            if (emp.getEmp_id() == employeeID.getFront()) {
+            if (emp.getEmp_id() == empWaitingList.getFront()) {
                 DMI.nextOrder(orderList.getFront());
             }
             orderList.dequeue();
-            employeeID.dequeue();
+            empWaitingList.dequeue();
         }
         try {
             Thread.sleep(100);
@@ -339,11 +341,11 @@ public class MainForm extends javax.swing.JFrame {
     }
 
     private void updateList() {
-        emp1 = new employee(100001, "jason@mail.com", "abcd1234", "offline", "890831-05-4492", "A-4-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4441221", "DM");
-        emp2 = new employee(100002, "jack@mail.com", "abcd1234", "other", "890731-05-4492", "A-7-4 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4661321", "EXEC");
-        emp3 = new employee(100003, "annabelle@mail.com", "abcd1234", "offline", "800831-05-4592", "A-3-6 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-8535221", "DM");
-        emp4 = new employee(100004, "marie@mail.com", "abcd1234", "offline", "990731-08-4492", "A-2-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-1231221", "DM");
-        emp5 = new employee(100005, "lucas@mail.com", "abcd1234", "other", "790821-05-4492", "A-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4990621", "AFFT");
+        emp1 = new employee(100001, "jason@mail.com", "abcd1234", "offline", "890831-05-4492", "A-4-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4441221", "DM" , 2009, 0);
+        emp2 = new employee(100002, "jack@mail.com", "abcd1234", "other", "890731-05-4492", "A-7-4 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4661321", "EXEC", 2010, 0);
+        emp3 = new employee(100003, "annabelle@mail.com", "abcd1234", "offline", "800831-05-4592", "A-3-6 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-8535221", "DM", 2016, 0);
+        emp4 = new employee(100004, "marie@mail.com", "abcd1234", "offline", "990731-08-4492", "A-2-2 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-1231221", "DM", 2012, 0);
+        emp5 = new employee(100005, "lucas@mail.com", "abcd1234", "other", "790821-05-4492", "A-6-5 Sri Pelangi, Jln Genting Klang, 53300 KL", "012-4990621", "AFFT", 2014, 0);
         att1 = new Attendance(600001, 100001, "21/07/2017", "08:00:21", "17:03:21", "13:10:52", "13:55:13");
         att2 = new Attendance(600002, 100002, "21/07/2017", "08:01:11", "17:13:31", "13:05:51", "14:02:11");
         att3 = new Attendance(600003, 100001, "22/07/2017", "08:11:31", "17:23:41", "13:12:25", "14:01:12");
