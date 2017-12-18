0d067c9a5f7f0aa493fbb3ae11303636695a8931
Mah Hong Wai <Mah Hong Wai@MSI>
2017-12-19 00:38:06 +0800

    Mah

---
+++ src/PART_A/ArrangeItem.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PART_A;
+
+import adt.AffiliateADT;
+import adt.LList;
+import entity.Affiliate;
+import entity.*;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.util.*;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Mah Hong Wai
+ */
+public class ArrangeItem extends JFrame{
+    
+   
+    int currentID = 1000;
+    AffiliateADT<Affiliate> affiliate=new AffiliateADT<Affiliate>();
+    Affiliate restA=new Affiliate(1000,"Rest A","Rest A address","Ali","0111111111","Zone A","123456789");
+    JButton btnMnly = new JButton("Manually");
+    JButton btnAuto = new JButton("Automatically");
+    JLabel lblTitle = new JLabel("Select the way to arrange");
+    JPanel P = new JPanel();
+    JLabel lblEmpty = new JLabel("       ");
+    Font FontTitle = new Font("SansSerif",Font.BOLD,30);
+    public ArrangeItem(){
+        affiliate.add(restA);
+        try{
+                 for (int i = 1; i < affiliate.getNumberOfEntries() + 1; i++) {
+                        if (affiliate.getEntry(i).getAffiliate_id() == currentID) {
+                            if(affiliate.getEntry(i).getItemList().isEmpty()){
+
+
+                            }
+                        }
+                 }
+                 
+            }
+                 catch (Exception e){
+                     JOptionPane.showMessageDialog(null, "No Item", "InfoBox: " + "Error!!", JOptionPane.ERROR_MESSAGE);
+                     System.exit(0);
+                 }
+        
+        btnAuto.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                SelAuto(evt);
+            }
+        });
+        
+        btnMnly.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                SelMnly(evt);
+            }
+        });
+        
+        lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitle.setFont(FontTitle);
+        btnAuto.setFont(FontTitle);
+        btnMnly.setFont(FontTitle);
+        add(lblTitle);
+        
+        setLayout(new FlowLayout(FlowLayout.CENTER,100,20));
+        add(btnAuto);
+        add(btnMnly);
+        setTitle("Arrange Item");
+        setSize(500, 250);  
+        setLocationRelativeTo(null);   
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+        setVisible(true);
+    }    
+    
+    private void SelAuto(ActionEvent evt) {
+            
+            this.dispose();
+            AutoArrange AA=new AutoArrange(currentID);
+        
+        }
+    
+    private void SelMnly(ActionEvent evt) {
+            
+            this.dispose();
+        
+        }
+    
+    public static void main(String[] args){
+        
+        ArrangeItem AI=new ArrangeItem();
+       
+    }
+}
---
+++ src/PART_C/Restaurant.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PART_C;
+/**
+ *
+ * @author Loi Kah Hou
+ */
+public class Restaurant {
+    String restName;
+    public Restaurant(String restName){
+        this.restName=restName;
+    }
+    
+    public String getRestName(){
+        return restName;
+    }
+}
+
